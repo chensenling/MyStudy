460e268616d62a182e4f4c41161fefc4112c5979
chensenling <maked_chensenling@163.com>
2020-11-28 23:48:13 +0800

    第二次提交

--- Demo01/src/main/java/com/mitan/Boot.java
+++ Demo01/src/main/java/com/mitan/Boot.java
@@ -4,6 +4,7 @@ public class Boot {
     public static void main(String[] args) {
         System.out.println("测试01");
         System.out.println("github 托管 在线更改");
+        System.out.println("本地idea 制造冲突");
         System.out.println("github 托管在线制造冲突");
     }
 }
