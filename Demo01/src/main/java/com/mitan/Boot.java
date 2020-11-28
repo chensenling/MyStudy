677c13349ad802a238ffe5a04ba5cdacdb75484d
lemon <44091325+chensenling@users.noreply.github.com>
2020-11-28 23:46:11 +0800

    Update Boot.java
    github 制造冲突

--- Demo01/src/main/java/com/mitan/Boot.java
+++ Demo01/src/main/java/com/mitan/Boot.java
@@ -4,5 +4,6 @@ public class Boot {
     public static void main(String[] args) {
         System.out.println("测试01");
         System.out.println("github 托管 在线更改");
+        System.out.println("github 托管在线制造冲突");
     }
 }
